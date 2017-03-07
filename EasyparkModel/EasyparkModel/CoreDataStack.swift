fb56646f30c9ba703e8c60c9b09581d7723770bd
Sebastien Bastide <sebastienbastide@imacdesebastien.home>
2017-03-07 12:51:26 +0100

    Implementation of EasyparkModel Framework and JSONManager.

---
+++ EasyparkModel/EasyparkModel/CoreDataStack.swift
@@ -0,0 +1,13 @@
+//
+//  CoreDataStack.swift
+//  EasyparkModel
+//
+//  Created by Sebastien Bastide on 06/03/2017.
+//  Copyright © 2017 Sebastien Bastide. All rights reserved.
+//
+
+import UIKit
+
+class CoreDataStack: NSObject {
+
+}
---
+++ EasyparkModel/EasyparkModel/Data/Entities/Parking.swift
@@ -0,0 +1,6 @@
+import Foundation
+
+@objc(Parking)
+open class Parking: _Parking {
+	// Custom logic goes here.
+}
